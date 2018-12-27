10837a46361a46655adb47fa75e1873c953dfcd4
Iryna_Vavruk <iryna_vavruk@epam.com>
2018-12-27 12:41:15 +0200

    new

---
+++ src/test/java/SpringBootGrowCrudRestfull/AppTest.java
@@ -0,0 +1,20 @@
+package SpringBootGrowCrudRestfull;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple SpringBootGrowCrudRestfull.App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
